21b1a529581867985f9c6242cda23b913ab13c10
sgz <24421237@qq.com>
2019-07-10 13:56:30 +0800

    springcloud demo

--- consumer/src/main/java/com/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/consumer/ConsumerApplication.java
@@ -2,16 +2,25 @@ package com.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @Author: xiecan
+ * @CreateDate: 2019/7/10 10:45
+ * @UpdateUser: xiecan
+ * @UpdateDate: 2019/7/10 10:45
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
+
 
 }
--- consumer/src/main/java/com/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/consumer/controller/ConsumerController.java
@@ -1,7 +1,10 @@
 package com.consumer.controller;
 
 import com.consumer.service.ProductService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,20 @@ public class ConsumerController {
     public String getConsumer() {
         String result = productService.getProduct();
         return result;
+    }
+
+    @RequestMapping("/getOne/{id}")
+    @HystrixCommand(fallbackMethod = "getOneFallBack", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")})
+    public String getOne(@PathVariable String id) {
+        int sum = 1 / 0;
+        return "true";
+    }
+
+    /**
+     * 参数跟返回类型必须跟上面的一样，不然会报找不到该方法的错
+     */
+    public String getOneFallBack(String id) {
+        return id;
     }
 }
